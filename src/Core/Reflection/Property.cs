4cd19e43e71255ee882cb6805ce1b7ad1c55e4a9
Vitaliy <vk@alphacloud.net>
2013-05-17 20:23:42 +0300

    Add unit-tests for Property class

--- src/Core/Reflection/Property.cs
+++ src/Core/Reflection/Property.cs
@@ -1,35 +1,61 @@
 ﻿namespace Alphacloud.Common.Core.Reflection
 {
-    #region using
-
     using System;
     using System.Linq.Expressions;
     using System.Text;
     using Data;
     using JetBrains.Annotations;
 
-    #endregion
-
     /// <summary>
-    ///   Gets property name using lambda expressions.
+    ///   Retrieves text property name from lambda expression.
     /// </summary>
+    /// <remarks>
+    ///   Nested properties are supported.
+    ///   Indexes are NOT supported.
+    /// </remarks>
     [PublicAPI]
     public static class Property
     {
+        /// <summary>
+        ///   Get property name from expression.
+        /// </summary>
+        /// <typeparam name="T">Type</typeparam>
+        /// <param name="obj">Object instance.</param>
+        /// <param name="expression">The expression.</param>
+        /// <returns>Property name.</returns>
         public static string PropertyName<T>(this T obj, Expression<Func<T, object>> expression)
         {
-            var body = expression.Body;
+            Expression body = expression.Body;
             return GetMemberName(new StringBuilder(128), body, false).ToString();
         }
 
+        /// <summary>
+        ///   Get property name from expression.
+        /// </summary>
+        /// <typeparam name="T">Type</typeparam>
+        /// <param name="expression">The expression.</param>
+        /// <returns>Textual property name.</returns>
         public static string Name<T>(
             Expression<Func<T, object>> expression)
         {
-            var body = expression.Body;
+            Expression body = expression.Body;
             return GetMemberName(new StringBuilder(128), body, false).ToString();
         }
 
 
+        /// <summary>
+        ///   Get property name from expression.
+        /// </summary>
+        /// <param name="expression">The expression.</param>
+        /// <param name="skipInstanceReference">
+        ///   if set to <c>true</c> instance variable won't be included in property name.
+        /// </param>
+        /// <returns></returns>
+        /// <example>
+        ///   <c>Property.Name(() => obj.Name, false)</c> will return <c>obj.Name</c>
+        ///   while
+        ///   <c>Property.Name(() => obj.Name, true)</c> will return <c>Name</c>
+        /// </example>
         public static string Name(
             Expression<Func<object>> expression, bool skipInstanceReference = true)
         {
@@ -39,32 +65,34 @@
 
         private static StringBuilder GetMemberName(StringBuilder sb, Expression expression, bool skipInstanceReference)
         {
-            var memberExpression = expression as MemberExpression;
-            if (memberExpression != null)
+            while (true)
             {
-                if (memberExpression.Expression.NodeType ==
-                    ExpressionType.MemberAccess)
+                var memberExpression = expression as MemberExpression;
+                if (memberExpression != null)
                 {
-                    GetMemberName(sb, memberExpression.Expression, skipInstanceReference);
-                    if (sb.Length > 0)
-                        sb.Append(".");
+                    if (memberExpression.Expression.NodeType == ExpressionType.MemberAccess)
+                    {
+                        GetMemberName(sb, memberExpression.Expression, skipInstanceReference);
+                        if (sb.Length > 0)
+                            sb.Append('.');
+                        return sb.Append(memberExpression.Member.Name);
+                    }
+                    if (skipInstanceReference && memberExpression.Expression.NodeType == ExpressionType.Constant)
+                        return sb;
                     return sb.Append(memberExpression.Member.Name);
                 }
-                if (skipInstanceReference && memberExpression.Expression.NodeType == ExpressionType.Constant)
-                    return sb;
-                return sb.Append(memberExpression.Member.Name);
-            }
 
-            var unaryExpression = expression as UnaryExpression;
-            if (unaryExpression != null)
-            {
+                var unaryExpression = expression as UnaryExpression;
+                if (unaryExpression == null)
+                    throw new InvalidOperationException(
+                        "Could not determine member from {0}".ApplyArgs(expression));
+
                 if (unaryExpression.NodeType != ExpressionType.Convert)
-                    throw new Exception("Cannot interpret member from {0}".ApplyArgs(expression.ToString()));
+                    throw new InvalidOperationException(
+                        "Cannot interpret member from {0}".ApplyArgs(expression));
 
-                return GetMemberName(sb, unaryExpression.Operand, skipInstanceReference);
+                expression = unaryExpression.Operand;
             }
-
-            throw new Exception("Could not determine member from {0}".ApplyArgs(expression.ToString()));
         }
     }
 }
---
+++ src/Tests/Core.Tests/Reflection/PropertyTests.cs
@@ -0,0 +1,83 @@
+﻿namespace Core.Tests.Reflection
+{
+    using System;
+    using System.Collections.Generic;
+    using Alphacloud.Common.Core.Reflection;
+    using FluentAssertions;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class PropertyTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            _obj = new Aggregate {NestedObj = new Nested()};
+        }
+
+        private Aggregate _obj;
+
+        private class Nested
+        {
+            public string Name { get; set; }
+        }
+
+        private class Aggregate
+        {
+            public int Id { get; set; }
+            public Nested NestedObj { get; set; }
+            public List<int> List { get; set; }
+        }
+
+        [Test]
+        public void Name_Should_HandleNastedProperties()
+        {
+            Property.Name<Aggregate>(a => a.NestedObj.Name)
+                .Should().Be("NestedObj.Name");
+        }
+
+        [Test]
+        public void Name_Should_HandleNestedPropertiesFromInstance()
+        {
+            Property.Name(() => _obj.NestedObj.Name, false)
+                .Should().Be("_obj.NestedObj.Name");
+        }
+
+
+        [Test]
+        [ExpectedException(typeof (InvalidOperationException))]
+        public void Name_Should_Not_HandleIndexers()
+        {
+            Property.Name<Aggregate>(a => a.List[0])
+                .Should().Be("List[0]");
+        }
+
+        [Test]
+        public void Name_Should_ReturnPropertyName()
+        {
+            Property.Name<Aggregate>(a => a.Id)
+                .Should().Be("Id");
+        }
+
+        [Test]
+        public void Name_Should_ReturnPropertyNameFromInstance()
+        {
+            Property.Name(() => _obj.Id)
+                .Should().Be("Id");
+        }
+
+        [Test]
+        public void PropertyName_Should_HandleNestedProperties()
+        {
+            _obj.PropertyName(o => o.NestedObj.Name)
+                .Should().Be("NestedObj.Name");
+        }
+
+        [Test]
+        public void PropertyName_Should_ReturnPropertyName()
+        {
+            _obj.PropertyName(o => o.Id)
+                .Should().Be("Id");
+        }
+    }
+}
