ab8c5f5269a1a188ebf72b3f29622ee4059c508b
Vitaliy <vk@alphacloud.net>
2013-05-21 19:19:57 +0300

    Add PrincipalProvider and IServiceFactory

--- src/Core/Utils/EventHelper.cs
+++ src/Core/Utils/EventHelper.cs
@@ -1,15 +1,34 @@
-﻿using System;
-using AccountView.Common.Base.Annotations;
+﻿#region copyright
 
-namespace AccountView.Common.Base.Data
+// Copyright 2013 Alphacloud.Net
+// 
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+// 
+//        http://www.apache.org/licenses/LICENSE-2.0
+// 
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
+#endregion
+
+namespace Alphacloud.Common.Core.Utils
 {
+    using System;
+    using JetBrains.Annotations;
+
     /// <summary>
-    ///     Event helper.
+    ///   Event helper.
     /// </summary>
+    [PublicAPI]
     public class EventHelper
     {
         /// <summary>
-        ///     Raise event if subscribers exist.
+        ///   Raise event if subscribers exist.
         /// </summary>
         /// <typeparam name="TEventArgs">Event arguments type.</typeparam>
         /// <param name="handler">Event handler.</param>
@@ -25,7 +44,7 @@ namespace AccountView.Common.Base.Data
         }
 
         /// <summary>
-        ///     Check
+        ///   Raise event if subscribers exist.
         /// </summary>
         /// <typeparam name="TEventArgs">Event arguments type.</typeparam>
         /// <param name="handler">Event handler.</param>
@@ -37,13 +56,11 @@ namespace AccountView.Common.Base.Data
             where TEventArgs : EventArgs
         {
             if (eventArgsBuilder == null) throw new ArgumentNullException("eventArgsBuilder");
-            if (handler != null)
-            {
-                TEventArgs eventArgs = eventArgsBuilder();
-                if (eventArgs == null)
-                    throw new InvalidOperationException("EventArgs builder returned null");
-                handler(sender, eventArgs);
-            }
+            if (handler == null) return;
+            var eventArgs = eventArgsBuilder();
+            if (eventArgs == null)
+                throw new InvalidOperationException("EventArgs builder returned null");
+            handler(sender, eventArgs);
         }
     }
 }
