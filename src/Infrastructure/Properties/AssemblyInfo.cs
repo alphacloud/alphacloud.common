1ff1a9129a6ed4cf4ca35832130e65cc4698fe2e
vk <vk@alphacloud.net>
2013-05-21 22:45:39 +0300

    Add Infrastructure project

---
+++ src/Infrastructure/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Infrastructure")]
+[assembly: AssemblyDescription("Alphacloud infrastructure")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ba321126-5c95-4959-9f5b-43c1b249a2ea")]
+
+[assembly: AssemblyVersion("0.0.1.1")]
+[assembly: AssemblyFileVersion("0.0.1.1")]
