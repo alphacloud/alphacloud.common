ad13267bb206384c07e051beca4472295a68e65d
shatl <vk@alphacloud.net>
2014-04-23 16:49:29 -0400

    Sample ASP.NET MVC application, see #5

---
+++ src/Samples/WebMvcSample/App_Start/Startup.cs
@@ -0,0 +1,77 @@
+﻿#region copyright
+
+// Copyright 2013-2014 Alphacloud.Net
+// 
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+// 
+//        http://www.apache.org/licenses/LICENSE-2.0
+// 
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
+#endregion
+
+using Microsoft.Owin;
+using WebMvcSample;
+
+[assembly: OwinStartup(typeof (Startup))]
+
+namespace WebMvcSample
+{
+    using System.Web.Mvc;
+    using Alphacloud.Common.Infrastructure.Instrumentation;
+    using Alphacloud.Common.Instrumentation.Log4Net;
+    using Alphacloud.Common.Web.Mvc.Instrumentation;
+    using Autofac;
+    using Autofac.Integration.Mvc;
+    using Common.Logging;
+    using Controllers;
+    using JetBrains.Annotations;
+    using Owin;
+
+
+    public class Startup
+    {
+        static readonly ILog s_log = LogManager.GetCurrentClassLogger();
+        internal static IContainer Container;
+
+
+        [UsedImplicitly]
+        public void Configuration(IAppBuilder app)
+        {
+            var cb = new ContainerBuilder();
+            ConfigureIoc(cb);
+            Container = cb.Build();
+
+            var dependencyResolver = new AutofacDependencyResolver(Container);
+            DependencyResolver.SetResolver(dependencyResolver);
+
+            ConfigureInstrumentation();
+        }
+
+
+        static void ConfigureInstrumentation()
+        {
+            var settings = new InstrumentationSettings(
+                new HttpInstrumentationContextProvider(),
+                new HttpCorrelationIdProvider(new Log4NetDiagnosticContext())
+                ) {
+                    Enabled = true,
+                };
+            InstrumentationRuntime.Instance.SetConfigurationProvider(() => settings);
+        }
+
+
+        static void ConfigureIoc(ContainerBuilder cb)
+        {
+            cb.RegisterControllers(typeof (HomeController).Assembly);
+            cb.RegisterModelBinders(typeof (HomeController).Assembly);
+            cb.RegisterModelBinderProvider();
+        }
+    }
+}
---
+++ src/Samples/WebMvcSample/Properties/AssemblyInfo.cs
@@ -0,0 +1,45 @@
+﻿#region copyright
+
+// Copyright 2013-2014 Alphacloud.Net
+// 
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+// 
+//        http://www.apache.org/licenses/LICENSE-2.0
+// 
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
+#endregion
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("Alphacloud Web Application Sample")]
+[assembly: AssemblyDescription("ASP.NET MVC + Web API sample")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Alphacloud.net")]
+[assembly: AssemblyProduct("WebMvcSample")]
+[assembly: AssemblyCopyright("Copyright © Alphacloud.Net 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("1a4238f3-4767-49f8-8384-e83907e4b735")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
