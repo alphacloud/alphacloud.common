d517e087d01758c5312ed98cca50730ab185c5b1
vk <vk@alphacloud.net>
2014-07-06 21:01:33 -0400

    Adding Redis adapter...

---
+++ src/Integration/Caching.Redis/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Alphaclud.Common.Caching.Redis")]
+[assembly: AssemblyDescription("Redis adapter for common caching infrastructure")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e995e036-ecd6-4dd8-801c-d8888b484aa1")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+
