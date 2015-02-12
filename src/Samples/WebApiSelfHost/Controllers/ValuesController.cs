6ff6efbe013a05a8635760aa3a07fbc8363bda87
shatl <vk@alphacloud.net>
2015-02-12 17:58:01 -0500

    Add WebAPI project, see #5

---
+++ src/Samples/WebApiSelfHost/Controllers/ValuesController.cs
@@ -0,0 +1,64 @@
+﻿#region copyright
+
+// Copyright 2013-2015 Alphacloud.Net
+// 
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+// 
+//        http://www.apache.org/licenses/LICENSE-2.0
+// 
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
+#endregion
+
+namespace WebApiSelfHost.Controllers
+{
+    using System.Collections.Generic;
+    using System.Web.Http;
+    using Common.Logging;
+
+    public class ValuesController : ApiController
+    {
+        static readonly ILog s_log = LogManager.GetLogger<ValuesController>();
+        // GET api/values 
+        public IEnumerable<string> Get()
+        {
+            s_log.Info("Loading values...");
+            return new[] {"value1", "value2"};
+        }
+
+
+        // GET api/values/5 
+        public string Get(int id)
+        {
+            s_log.InfoFormat("Loading value {0}", id);
+            return "value" + id;
+        }
+
+
+        // POST api/values 
+        public void Post([FromBody] string value)
+        {
+            s_log.InfoFormat("Adding value '{0}'", value);
+        }
+
+
+        // PUT api/values/5 
+        public void Put(int id, [FromBody] string value)
+        {
+            s_log.InfoFormat("Updating value {0} with '{1}'", id, value);
+        }
+
+
+        // DELETE api/values/5 
+        public void Delete(int id)
+        {
+            s_log.InfoFormat("Deleting value {0}", id);
+        }
+    }
+}
---
+++ src/Samples/WebApiSelfHost/Properties/AssemblyInfo.cs
@@ -0,0 +1,57 @@
+﻿#region copyright
+
+// Copyright 2013-2015 Alphacloud.Net
+// 
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+// 
+//        http://www.apache.org/licenses/LICENSE-2.0
+// 
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
+#endregion
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("WebApiSelfHost")]
+[assembly: AssemblyDescription("WebAPI self-hosting app")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("AlphaCloud")]
+[assembly: AssemblyProduct("WebApiSelfHost")]
+[assembly: AssemblyCopyright("Copyright © AlphaCloud 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("3266e493-62b1-4059-8e5c-b12f0351b9b7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
