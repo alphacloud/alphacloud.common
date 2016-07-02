d432064c37612bb37ccd869c737ecdd6572d0310
vk <vk@alphacloud.net>
2016-07-02 11:09:02 -0400

    Update nuspecs

--- src/Web/Web.Mvc.Shared/Properties/AssemblyInfo.cs
+++ src/Web/Web.Mvc.Shared/Properties/AssemblyInfo.cs
@@ -36,5 +36,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("996c3884-af52-486f-a26c-832669005fe3")]
 
-[assembly: AssemblyVersion("0.3.0.0")]
-[assembly: AssemblyFileVersion("0.3.0.0")]
+[assembly: AssemblyVersion("0.4.0.0")]
+[assembly: AssemblyFileVersion("0.4.0.0")]
